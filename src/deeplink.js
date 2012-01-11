65bba5ae5e75761fd02d49ceb585154132c0df61
Matthew Cobbs <draeton@gmail.com>
2012-01-11 16:46:27 -0500

    first commit

---
+++ src/deeplink.js
@@ -0,0 +1,14 @@
+// ## deeplink
+// 
+// 
+// [http://draeton.github.com/deeplink](http://draeton.github.com/deeplink)
+//
+// Copyright 2011, Matthew Cobbs  
+// Licensed under the MIT license.
+//
+/*global jQuery*/
+(function (window, $) {
+
+    "use strict";
+
+})(window, jQuery);
